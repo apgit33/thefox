1f00a17242acdbd42cc6ef86d4a285ba1568a225
apgit33 <adrienpaturot@yahoo.fr>
2020-05-19 15:11:30 +0200

    Update like onclick
    Ajout d'une fonction onClick sur les coeurs, change l'icone et implémente le nombre de like (ou diminue au dislike)

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-const likeMe = document.getElementById("like");
-const classLike = document.getElementsByClassName("fa-heart");
+$(".fa-heart").on('click', function(event){
+    // event.stopPropagation();
+    // event.stopImmediatePropagation();
+    let cpt = $(this).next().text();
 
-var cpt = document.getElementById("number").innerHTML; // Initialisation du compteur
-var node =  document.getElementById("number"); // On récupère notre noeud où sera rafraîchi la valeur du compteur
-
-
-
-// likeMe.addEventListener('click', function() {
-//     this.className = "fas fa-heart";
-//     if (this.style.color != "rgb(26, 188, 156)") {
-//         this.style.color = "rgb(26, 188, 156)";
-//         node.innerHTML = ++cpt;
-//     } else {
-//         this.style.color = "";
-//         this.className = "far fa-heart";
-//         node.innerHTML = --cpt;
-//     }
-
-// })
-
-
-
-$(".coeur").on('click', function(event){
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-    //(... rest of your JS code)
-    this.className = 'fas fa-heart coeur';
-    console.log(this);
-
+    if (this.className == 'far fa-heart') { //like
+        this.className = 'fas fa-heart';
+        this.style.color = 'rgb(26, 188, 156)';
+        $(this).next().text(++cpt);
+    } else {    //dislike
+        this.className = 'far fa-heart';
+        this.style.color = "";
+        $(this).next().text(--cpt);
+    }
 });
+
+
