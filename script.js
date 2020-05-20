b5c0cafd3aa004bb68977561f84aa129b54bdf63
apgit33 <adrienpaturot@yahoo.fr>
2020-05-20 19:26:40 +0200

    Update script

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
 $(".fa-heart").on('click', function(){
+    // initialisation d'un compteur avec la valeur du texte de la balise voisine
     let cpt = $(this).next().text();
 
-    if (this.className == 'far fa-heart') { //like
-        this.className = 'fas fa-heart';
-        this.style.color = 'rgb(26, 188, 156)';
-        $(this).next().text(++cpt);
+    if (this.className == 'far fa-heart') { //
+        this.className = 'fas fa-heart'; //on change d'icone
+        this.style.color = 'rgb(26, 188, 156)'; //on lui applique la couleur
+        $(this).next().text(++cpt); //on incrémente le nombre de like
     } else {    //dislike
         this.className = 'far fa-heart';
         this.style.color = "";
