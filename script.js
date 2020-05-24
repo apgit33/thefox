17e44e23b77fb94ce0fcc02bb4524ae2387046f6
Sawkea <shoareau@orange.fr>
2020-05-24 21:12:24 +0200

    updates responsive

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ $(".fa-heart").on('click', function(){
 });
 
 
+
+// Menu ---------------------------------------------------
+
+"use strict";
+window.addEventListener("DOMContentLoaded", (event) => {
+  /* MENU */
+  const lemenu = document.getElementById("lemenu");
+  const cmdmenu = document.getElementById("cmdmenu");
+  cmdmenu.addEventListener('click',function(){
+    lemenu.style.display = (lemenu.style.display == 'none')? '':'none';
+  });
+  // au chargement de la page
+  window.onload = function(){
+    // on teste la largeur de la fenêtre
+    var ww = window.innerWidth; // en pixels
+    lemenu.style.display = ( ww > 991 )? '':'none';
+    cmdmenu.style.display = ( ww > 991 )? 'none':'';
+  };
+  // au redimensionnement de la fenêtre
+  window.onresize = function(){
+    // on teste la largeur de la fenêtre
+    var ww = window.innerWidth; // en pixels
+    leMenu.style.display = ( ww > 991 )? '':'none';
+    cmdmenu.style.display = ( ww > 991 )? 'none':'';
+  };
+
+});
